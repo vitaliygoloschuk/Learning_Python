490cd6c35fca1da5bcf5e9ae59e89d569d22257e
vitaliygoloschuk <vitaliyprorok@gmail.com>
2022-10-08 17:42:54 +0300

    fix SQl

--- HW-4_SQL-1/school.sql
+++ HW-4_SQL-1/school.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
 -- Host: localhost    Database: school
 -- ------------------------------------------------------
--- Server version	8.0.30-0ubuntu0.22.04.1
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -38,6 +38,31 @@ LOCK TABLES `classes` WRITE;
 /*!40000 ALTER TABLE `classes` DISABLE KEYS */;
 INSERT INTO `classes` VALUES (1,'1-a',1),(2,'2-a',2),(3,'7-a',3),(4,'8-a',4);
 /*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lessons`
+--
+
+DROP TABLE IF EXISTS `lessons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lessons` (
+  `lessons_id` int NOT NULL AUTO_INCREMENT,
+  `techr_id` int NOT NULL,
+  `rms_id` int NOT NULL,
+  PRIMARY KEY (`lessons_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lessons`
+--
+
+LOCK TABLES `lessons` WRITE;
+/*!40000 ALTER TABLE `lessons` DISABLE KEYS */;
+INSERT INTO `lessons` VALUES (1,1,1),(2,1,3),(3,2,4),(4,2,1),(5,3,3);
+/*!40000 ALTER TABLE `lessons` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -140,6 +165,14 @@ LOCK TABLES `teachers` WRITE;
 INSERT INTO `teachers` VALUES (1,'Kateryna','Samotiy','samotiy@gmail.com'),(2,'Victoriya','Sagaydachna','sagaydachna@gmail.com'),(3,'Natalia','Khmelnutska','khmelnutska@gmail.com'),(4,'Anna','Mikhaylova','mm@gmail.com'),(5,'Irina','Mikhaylova','ira@test.com');
 /*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'school'
+--
+
+--
+-- Dumping routines for database 'school'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -150,4 +183,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-30 20:42:30
+-- Dump completed on 2022-10-08  3:31:45
