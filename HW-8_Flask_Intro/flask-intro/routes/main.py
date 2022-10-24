b9f696535255607312003f28f5adf30f16b93ab8
vitaliygoloschuk <vitaliyprorok@gmail.com>
2022-10-25 01:00:47 +0300

    solution for HW-8

--- HW-8_Flask_Intro/flask-intro/routes/main.py
+++ HW-8_Flask_Intro/flask-intro/routes/main.py
@@ -1,11 +1,12 @@
 from app import app
 
+
 @app.route("/")
 def hello():
     return "hello world!!!"
 
 
-@app.route("/calculation/<number>")
-
-
-
+@app.route("/calc/<number>/<number_two>")
+def calc(number, number_two):
+    result = int(number) + int(number_two)
+    return str(result)
