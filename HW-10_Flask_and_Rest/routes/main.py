39d0a77971b5d36e4cf9836a461d2c73a94ef92c
vitaliygoloschuk <vitaliyprorok@gmail.com>
2022-11-08 00:33:49 +0200

    A few details

--- HW-10_Flask_and_Rest/routes/main.py
+++ HW-10_Flask_and_Rest/routes/main.py
@@ -5,6 +5,8 @@ from models.models import Plant, Employee
 
 @app.route("/")
 def main():
-    plants = Plant.query.order_by(Plant.title.asc()).all()
-    print(plants)
+    # plants = Plant.query.order_by(Plant.title.asc()).all()
+    # plants = Plant.query.filter(Plant.location == "st. Kyiv 9") #(віддавався лише елемент)
+    plants = Plant.query.all()
+    # print(plants)
     return render_template("index.html", plants=plants, )
