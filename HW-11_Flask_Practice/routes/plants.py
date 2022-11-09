d16580bede562a4f202f87e7992acb10af53e1cb
vitaliygoloschuk <vitaliyprorok@gmail.com>
2022-11-09 23:46:18 +0200

    fix plant.py

--- HW-11_Flask_Practice/routes/plants.py
+++ HW-11_Flask_Practice/routes/plants.py
@@ -1,19 +1,25 @@
 from app import app, db
-from models.models import Plant
 from flask import render_template, request, redirect
+from models.models import Plant, Employee
 
 
 @app.route("/add-plant")
 def add_plant():
-    return render_template("add_plant.html")
+    employees = Employee.query.all()
+    return render_template("add_plant.html", employees=employees)
 
 
 @app.route("/save-plant", methods=["POST"])
 def save_plant():
+    # print(request.form.getlist("employees"))
     name = request.form.get("name")
     location = request.form.get("location")
     plant = Plant(title=name, location=location)
     db.session.add(plant)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.plant_id = plant.id
+        db.session.add(employee)
     db.session.commit()
     return redirect("/")
 
@@ -26,22 +32,23 @@ def delete_plant(id):
     db.session.commit()
     return redirect("/")
 
+
 @app.route("/edit-plant/<int:id>")
 def edit_plant(id):
     plant = Plant.query.get(id)
-    return render_template("add_plant.html", plant=plant)
+    employees = Employee.query.all()
+    return render_template("add_plant.html", plant=plant, employees=employees)
 
-@app.route("/update-plant/<int:id>", methods = ["POST"])
+
+@app.route("/update-plant/<int:id>", methods=["POST"])
 def update_plant(id):
     plant = Plant.query.get(id)
     plant.title = request.form.get("name")
     plant.location = request.form.get("location")
     db.session.add(plant)
+    for employee_id in request.form.getlist("employees"):
+        employee = Employee.query.get(int(employee_id))
+        employee.plant_id = plant.id
+        db.session.add(employee)
     db.session.commit()
     return redirect("/")
-
-@app.route("/inform-plant/<int:id>")
-def inform_plant(id):
-    inf_plant = Plant.query.get(id)  # конкретний завод
-    print(Plant.title)
-    return render_template("inform-plant.html", inf_plant=inf_plant)
