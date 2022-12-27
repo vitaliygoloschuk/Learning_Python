1c44afbfcef0c7bf258b84d65bcafb5ff8feeb04
vitaliygoloschuk <vitaliyprorok@gmail.com>
2022-12-28 00:13:37 +0200

    modify name of variables

--- HW-9_Flask_Practice/routes/plants.py
+++ HW-9_Flask_Practice/routes/plants.py
@@ -26,6 +26,6 @@ def delete_plant(id):
 
 @app.route("/inform-plant/<int:id>")
 def inform_plant(id):
-    inf_plant = Plant.get_by_id_for_flask(id)  # конкретний завод
-    inf_emploe = Plant.get_by_id_for_flask_empl(id)
+    inf_plant = Plant.get_by_id_pl(id)  # конкретний завод
+    inf_emploe = Plant.get_by_id_empl(id)
     return render_template("inform-plant.html", inf_plant=inf_plant, inf_emploe=inf_emploe)
